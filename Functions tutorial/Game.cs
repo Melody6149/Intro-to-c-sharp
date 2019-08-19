82dfa9a26fce4c354188266bf60eece4132f9eae
Reid Eastin <s198012@students.aie.edu>
2019-08-19 17:33:16 -0500

    did functions tutorial

---
+++ Functions tutorial/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_tutorial
+{
+    class Game
+    {
+        int numberOne = 10;
+        int numberTwo = 200;
+
+        public void start()
+        {
+            int biggestnumber = ReturnBiggestNumber(numberOne, numberTwo);
+            Console.WriteLine("Of " + numberOne + " and " + numberTwo + " the biggest number is " + biggestnumber);
+            Console.ReadKey();
+        }
+        int ReturnBiggestNumber(int first, int second)
+        {
+            if(first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
