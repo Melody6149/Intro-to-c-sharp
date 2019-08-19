6bb9e747fe8c752646d804c0f079f1520c6153e9
Reid Eastin <s198012@students.aie.edu>
2019-08-19 15:01:45 -0500

    Add project files.

---
+++ Myfirstprogram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myfirstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output text//
+            Console.WriteLine("Hello. What is your name?");
+            string playername = Console.ReadLine();
+            // allows you to type in name//
+
+            int playerscore = 0;
+           
+            int add = 0;
+            
+            while (playerscore < 10)
+            {
+                Console.Write("increment score: ");
+                add = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Score of " + playername);
+                // adds to score//
+                playerscore = playerscore + add;
+                Console.WriteLine(playerscore);
+                //dislplays score make sure to put it after i add to score//
+                
+            }
+            
+        }
+    }
+}
